c8c53c8dc50d4418e572280236a9dc84dc2b2de9
Shoxzod <Shoxzodallayarov@gmail.com>
2024-03-20 13:31:42 +0500

    s

--- backend/admin.py
+++ backend/admin.py
@@ -1,9 +1,14 @@
 # backend/admin.py
 
 from django.contrib import admin
-from .models import News
+from .models import News, Template, Template2Button
 
-class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content',)
-    search_fields = ('title', 'content',)
+@admin.register(Template)
+class TemplateAdmin(admin.ModelAdmin):
+    list_display = ('title', 'body_en', 'body_ru', 'body_uz')
+    search_fields = ('title', 'body_en', 'body_ru', 'body_uz')
 
+@admin.register(Template2Button)
+class Template2ButtonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'body_en', 'body_ru', 'body_uz')
+    search_fields = ('title', 'body_en', 'body_ru', 'body_uz')
--- backend/models.py
+++ backend/models.py
@@ -8,3 +8,29 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
+
+class Template(models.Model):
+    title = models.CharField(max_length=255, unique=True, verbose_name='Ключь')
+    body_en = models.TextField(verbose_name='English')
+    body_ru = models.TextField(verbose_name='Русский')
+    body_uz = models.TextField(verbose_name='Uzbekcha')
+
+    class Meta:
+        verbose_name = 'Шаблон'
+        verbose_name_plural = 'Шаблоны'
+
+    def __str__(self):
+        return self.title
+
+class Template2Button(models.Model):
+    title = models.CharField(max_length=255, unique=True, verbose_name='Ключь')
+    body_en = models.TextField(verbose_name='English')
+    body_ru = models.TextField(verbose_name='Русский')
+    body_uz = models.TextField(verbose_name='Uzbekcha')
+
+    class Meta:
+        verbose_name = 'Небольшой шаблон'
+        verbose_name_plural = 'Небольшие шаблоны'
+
+    def __str__(self):
+        return self.title
