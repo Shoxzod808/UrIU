baeeea1eade384e818c3a8aa80e0da56d5034902
azdd <160459052+AzdCreations@users.noreply.github.com>
2024-03-20 14:02:29 +0500

    404

--- backend/views.py
+++ backend/views.py
@@ -6,4 +6,4 @@ def index(request, language='uz'):
     if language in ['ru', 'en', 'uz']:
         return render(request, 'frontend/home-1.html')
     else:
-        return render(request, 'frontend/site/404.html')
+        return render(request, 'frontend/404.html')
