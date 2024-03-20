4a31d9a81ed0d5ef0d2d63cd147133881eb69925
Shoxzod <Shoxzodallayarov@gmail.com>
2024-03-20 13:52:43 +0500

    404 views

--- backend/views.py
+++ backend/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return render(request, 'frontend/home-1.html')
+def index(request, language='uz'):
+    if language in ['ru', 'en', 'uz']:
+        return render(request, 'frontend/home-1.html')
+    else:
+        return render(request, 'frontend/site/404.html')
